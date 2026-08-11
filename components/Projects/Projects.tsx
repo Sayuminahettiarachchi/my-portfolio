d4e80fff2017f39e4f0ee4697e2a16ec8216079e
sayumina <sayumina2020@gmail.com>
2026-08-11 22:11:07 +0530

    Add AI career recommendation project

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -3,106 +3,48 @@ import ProjectCard from "./ProjectCard";
 console.log("PROJECT FILE LOADED");
 
 export default function Projects() {
-
   const projects = [
-
     {
       image: "/projects/Meteorite_Landings.png",
       title: "Meteorite Landings Analysis",
       type: "AI / MACHINE LEARNING",
-      github: "https://github.com/Sayuminahettiarachchi/Meteorite_Landings",
+      github:
+        "https://github.com/Sayuminahettiarachchi/Meteorite_Landings",
       live: "#",
     },
-
     {
-      image: "/projects/robot.png",
-      title: "Smart Trash Classification Robot",
-      type: "IOT / ROBOTICS",
-      github: "https://github.com/yourusername/smart-trash-robot",
-      live: "#",
+      image: "/projects/Screenshot (917).png",
+      title: "AI Career Recommendation System",
+      type: "AI / MACHINE LEARNING / WEB",
+      github:
+        "https://github.com/Sayuminahettiarachchi/AI-Career-Recommendation",
+      live: "https://sayumina.pythonanywhere.com/",
     },
-
-    {
-      image: "/projects/portfolio.png",
-      title: "Personal Portfolio Website",
-      type: "WEB DEVELOPMENT",
-      github: "https://github.com/yourusername/my-portfolio",
-      live: "#",
-    },
-
   ];
 
-
   return (
-
     <section
       id="projects"
-      className="
-      min-h-screen
-      bg-[#020617]
-      text-white
-      px-6
-      py-16
-      "
+      className="min-h-screen bg-[#020617] text-white px-6 py-16"
     >
-
       <div className="max-w-6xl mx-auto">
-
-
-        <h2
-          className="
-          text-5xl
-          font-bold
-          text-center
-          mb-12
-          "
-        >
-
-          MY <span className="text-cyan-400">
-            PROJECTS
-          </span>
-
+        <h2 className="text-5xl font-bold text-center mb-12">
+          MY <span className="text-cyan-400">PROJECTS</span>
         </h2>
 
-
-
-        <div
-          className="
-          grid
-          md:grid-cols-3
-          gap-8
-          "
-        >
-
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {projects.map((project, index) => (
-
             <ProjectCard
-
               key={index}
-
               image={project.image}
-
               title={project.title}
-
               type={project.type}
-
               github={project.github}
-
               live={project.live}
-
             />
-
           ))}
-
-
         </div>
-
-
       </div>
-
-
     </section>
-
   );
-
 }
